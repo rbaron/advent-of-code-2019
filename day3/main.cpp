51165d4d644a78dd4173a8b08f3ccd56c63fa86f
rbaron <rbaron@rbaron.net>
2019-12-03 11:19:08 +0100

    Day 3 Part 2

--- day3/main.cpp
+++ day3/main.cpp
@@ -3,7 +3,7 @@
 #include <regex>
 #include <vector>
 #include <fstream>
-#include <set>
+#include <map>
 
 
 using Coord = std::pair<int,int>;
@@ -24,8 +24,8 @@ std::vector<Maneuver> decode_path(std::string& path) {
 
    for (; reg_it != reg_end; reg_it++) {
       maneuvers.push_back(Maneuver(
-         ((*reg_it)[1].str())[0], // First char of the first regex group
-         std::stoi((*reg_it)[2])  // Conversion to int of the second regex group
+         ((*reg_it)[1].str())[0], // First char of the first regex group (one of U, D, L, R)
+         std::stoi((*reg_it)[2])  // Conversion to int of the second regex group (number of steps)
       ));
    }
    return maneuvers;
@@ -76,40 +76,61 @@ int main() {
    file >> path1;
    file >> path2;
 
-   std::set<Coord> grid;
+   std::map<Coord,Steps> grid;
 
-   // Fill up grip with wire 1 positions
+   // Fill up grid with wire 1 positions
+   Steps wire1steps = 0;
    Coord wire1pos(0,0);
    for (auto maneuver : decode_path(path1)) {
       wire1pos = for_each_visited_coord(
          wire1pos,
          maneuver,
-         [&grid](const Coord& current){ grid.insert(current); });
+         [&grid, &wire1steps](const Coord& current){
+            wire1steps++;
+            if (grid.find(current) == grid.end()) {
+               grid[current] = wire1steps;
+            }
+         });
    }
 
-   std::set<Coord> intersections;
+   std::map<Coord,Steps> intersections;
 
    // Follow wire 2 until it visits a grid cell in which wire 1 is present
+   Steps wire2steps = 0;
    Coord wire2pos(0,0);
    for (auto maneuver : decode_path(path2)) {
       wire2pos = for_each_visited_coord(
          wire2pos,
          maneuver,
-         [&grid, &intersections](const Coord& current){
+         [&grid, &intersections, &wire2steps](const Coord& current){
+            wire2steps++;
             if (grid.find(current) != grid.end()) {
-               intersections.insert(current);
+               intersections[current] = grid[current] + wire2steps;
             }
          });
    }
 
-   auto closest_intersection = *std::min_element(
+   // Part 1 - min intersection by distance to (0,0)
+   auto p1_intersection = *std::min_element(
       intersections.begin(),
       intersections.end(),
-      [](Coord c1, Coord c2){
-         return manhattan(c1) < (manhattan(c2));
+      [](std::pair<Coord,Steps> p1, std::pair<Coord,Steps> p2){
+         return manhattan(p1.first) < manhattan(p2.first);
       }
    );
+   std::cout << "Part 1\n";
+   std::cout << "Intersection @ (" << p1_intersection.first.first << "," << p1_intersection.first.second << ")\n";
+   std::cout << "Distance: " << manhattan(p1_intersection.first) << std::endl;
 
-   std::cout << "Closest intersection: (" << closest_intersection.first << "," << closest_intersection.second << ")\n";
-   std::cout << "Distance: " << manhattan(closest_intersection) << std::endl;
+   // Part 1 - min intersection sum of steps
+   auto p2_intersection = *std::min_element(
+      intersections.begin(),
+      intersections.end(),
+      [](std::pair<Coord,Steps> p1, std::pair<Coord,Steps> p2){
+         return p1.second < p2.second;
+      }
+   );
+   std::cout << "Part 2\n";
+   std::cout << "Intersection @ (" << p2_intersection.first.first << "," << p2_intersection.first.second << ")\n";
+   std::cout << "Sum of steps: " << p2_intersection.second << std::endl;
 }
