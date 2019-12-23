b3a99a308d912d9a6860c8a08d58be4c440c9f58
rbaron <rbaron@rbaron.net>
2019-12-23 11:00:19 +0100

    Day 22 Part 1 - shuffling indices instead of whole decks

--- day22/main.cpp
+++ day22/main.cpp
@@ -1,40 +1,27 @@
 #include <algorithm>
+#include <bitset>
 #include <fstream>
 #include <iostream>
 #include <numeric>
 #include <regex>
 #include <vector>
+#include <unordered_set>
 
-using deck_t = std::vector<short int>;
+using deck_t = std::vector<uint64_t>;
 
-void deal_into_new_stack(deck_t& deck) {
-    auto size = deck.size();
-    for (int i = 0; i < size / 2; i++)
-        std::swap(deck[i], deck[size - 1- i]);
+size_t deal_into_new_stack(size_t position, size_t size) {
+    return size - 1 - position;
 }
 
-void cut(deck_t& deck, int n) {
-    auto copy(deck);
-    auto size = deck.size();
-    auto i = (n + size) % size;
-    std::copy_n(copy.begin() + i, size - i, deck.begin());
-    std::copy_n(copy.begin(), i, deck.begin() + size - i);
+size_t cut(size_t n, size_t position, size_t size) {
+    return (position - n + size) % size;
 }
 
-void deal_with_increment(deck_t& deck, int n) {
-    auto copy(deck);
-    auto size = deck.size();
-    for (int i = 0; i < size; i++)
-        deck[(i * n) % size] = copy[i];
+size_t deal_with_increment(size_t n, size_t position, size_t size) {
+    return (position * n) % size;
 }
 
-void print_deck(const deck_t& deck) {
-    for (const auto& card : deck)
-        std::cout << card << ", ";
-    std::cout << std::endl;
-}
-
-void shuffle(deck_t& deck, const std::string& method) {
+size_t map_position(size_t position, size_t size, const std::string& method) {
     const static std::regex \
         dins("deal into new stack"), \
         c("cut (-?\\d+)"),
@@ -44,25 +31,28 @@ void shuffle(deck_t& deck, const std::string& method) {
     std::smatch matches;
 
     if (std::regex_search(method.begin(), method.end(), matches, dins)) {
-        deal_into_new_stack(deck);
+        return deal_into_new_stack(position, size);
     } else if (std::regex_search(method.begin(), method.end(), matches, dwi)) {
-        deal_with_increment(deck, std::stoi(matches[1]));
+        return deal_with_increment(std::stoi(matches[1]), position, size);
     } else if (std::regex_search(method.begin(), method.end(), matches, c)) {
-        cut(deck, std::stoi(matches[1]));
+        return cut(std::stoi(matches[1]), position, size);
     } else {
         throw std::runtime_error("Unknown suffle method: " + method);
     }
-
 }
 
-int main() {
-    deck_t deck(10007);
-    std::iota(deck.begin(), deck.end(), 0);
+void part1() {
+    constexpr size_t size = 10007;
+    size_t pos = 2019;
 
     std::fstream file("input.txt");
     std::string line;
-    while (std::getline(file, line)) shuffle(deck, line);
 
-    auto it = find(deck.begin(), deck.end(), 2019);
-    std::cout << "Part 1: " << std::distance(deck.begin(), it) << std::endl;
+    while (std::getline(file, line)) pos = map_position(pos, size, line);
+
+    std::cout << "Part 1: " << pos << std::endl;
+}
+
+int main() {
+    part1();
 }
